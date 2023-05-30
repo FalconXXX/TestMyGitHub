6c843ba34716d36e431b1ecc31b016248ac5968c
FalconXXX <wheasel13@gmail.com>
2023-05-30 11:34:16 +0200

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,8 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println(" Commit ");
 		System.out.println(" Feature THREE blabla");
+		System.out.println("Commit");
+		System.out.println("Commit FEATURE TWO");
+
 	}
 
 }
