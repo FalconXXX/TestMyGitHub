d1286f2edd15312d8e55398fbf102efc7b25531a
FalconXXX <F4lc0nX@outlook.com>
2023-05-30 11:26:20 +0200

    TEST Master

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Commit ");
+		System.out.println(" Feature THREE blabla");
 	}
 
 }
